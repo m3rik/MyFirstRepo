b13155e4afd435c375b5d1ba878ed027bb8d2523
Urziceanu Paul <paul.urziceanu@gmail.com>
2012-10-22 03:04:11 +0300

    Added class interogare with ask method on Trie!

---
+++ Tema 1 POO/src/Interogare.java
@@ -0,0 +1,61 @@
+import java.util.StringTokenizer;
+
+
+public class Interogare {
+	private Trie t;
+	private Trie aux;
+	
+	public Interogare(Trie t)
+	{
+		this.t=t;
+		aux=new Trie();
+	}
+	
+	private ArrayOfNumbers evaluate(String s)
+	{
+		StringTokenizer st=new StringTokenizer(s);
+		ArrayOfNumbers a,b;
+		String aName,operator,bName;
+		try
+		{
+			aName=st.nextToken();
+			operator=st.nextToken();
+			bName=st.nextToken();
+			a=t.findKey(aName);
+			if(a==null)a=aux.findKey(aName);
+			b=t.findKey(bName);
+			if(b==null)b=aux.findKey(bName);
+			
+			if((a==null||b==null) && operator.equals("and"))return null;
+			if(a==null&&operator.equals("or")) return b;
+			if(b==null&&operator.equals("or")) return a;
+			
+			if(operator.equals("and"))a.AND(b);
+			else a.OR(b);
+			
+			
+			
+		}
+		catch (Exception e)
+		{
+			System.out.println("Exceptie: " + e);
+		}
+		return null;
+		
+	}
+	
+	public ArrayOfNumbers ask(String s)
+	{
+		int index=0;
+		if(s.lastIndexOf('(')==-1)
+			return t.findKey(s);
+	
+		while(s.lastIndexOf('(')!=-1)
+		{
+			int b=s.lastIndexOf('('), e=s.indexOf(')',b);
+			aux.addKey("_"+index, evaluate(s.substring(b+1,e-1)));
+			s=s.substring(0,b)+"_"+index +s.substring(e+1, s.length());
+		}
+		return aux.findKey(s);
+	}
+}
